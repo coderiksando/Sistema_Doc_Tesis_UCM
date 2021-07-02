4123ca0052ec000a4b2d790f74f47dd3592eed11
SavioCampos <saviocamposalbornoz@gmail.com>
2021-07-02 13:20:31 -0400

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
         $this->call([RolePermissionSeeder::class]);
         $this->call([UserRoleSeeder::class]);
         $this->call([AreasTesisTableSeeder::class]);
-        //$this->call([VinculacionesTableSeeder::class]);
+        $this->call([VinculacionesTableSeeder::class]);
         $this->call([RolePermissionAlumnoSeeder::class]);
         $this->call([RolePermissionCoordinadorSeeder::class]);
         $this->call([RolePermissionProfesorSeeder::class]);
