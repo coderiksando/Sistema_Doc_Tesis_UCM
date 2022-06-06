86a89809d73b0889dacf51d6354149b51a8a1162
èriksandovalsepulveda@gmail.com <eriksandovalsepulveda@gmail.com>
2022-06-06 17:36:24 -0400

    Update bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -20,9 +20,8 @@ try {
  */
 
 window.axios = require('axios');
-window.axios.defaults.baseURL = 'http://www.eici.ucm.cl/sigad/';     // cambiar la url base con su subdominio ej 'http://www.ejemplo.com/subdominio'
+window.axios.defaults.baseURL = '/sigad';     // cambiar la url base con su subdominio ej 'http://www.ejemplo.com/subdominio'
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-// window.axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
